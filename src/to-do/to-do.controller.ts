67b5816d7401bd452df82c1e8dc3c23a705e8894
khammami-dev <khouloud.hammami@myu.universitecentrale.tn>
2021-02-10 20:48:17 +0100

    'crud'

--- src/to-do/to-do.controller.ts
+++ src/to-do/to-do.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 import { TodoStatusEnum } from './enums/TodoStatusEnum';
 import { v4 as uuidv4 } from 'uuid';
 import { Todo } from './modele/todo';
@@ -25,16 +25,60 @@ export class ToDoController {
        */
     const {name, description} = todoData;
     const todo = new Todo();
+    todo.id = uuidv4();
     todo.description = description;
     todo.name = name;
     todo.date = new Date();
     todo.status = TodoStatusEnum.waiting;
-    todo.id = uuidv4;
+
     this.todos.push(todo);
     return todo;
   }
-  @Delete(':id')
-  DeleteTodo(){
+  @Get(':id')
+  getTodoById(
+    @Param('id') id: string  //récupere le id avec @param dans la variable id de type string
+  ): Todo   {
+   return  this.todos.find(
+       (todo : Todo) => todo.id === id);
+  }
+  @Delete('delete/:id')
+  DeletTodoById(
+    @Param('id') id:string): String {
+    const todo = this.todos.find((todo: Todo) => todo.id === id);
+    const todoIndex = this.todos.findIndex((todo: Todo) => todo.id === id);
+    if (todoIndex > 0) {
+      this.todos.splice(todoIndex, 1);
+      return 'suppression avec succès ';
+    } else {
+      return  "cette todo n'existe pas";
+    }
 
   }
+  @Patch('update/:id')
+  ModifierTodo(@Param('id') id, @Body() todoData): Todo{
+    const todo = this.todos.find((todo : Todo) => todo.id === id);
+    if(todo != todoData){
+      todo.name = todoData.name;
+      todo.description=todoData.description;
+    }
+    return todo;
+
+  }
+
+
+  @Put('updateP/:id')
+  ModifierPartieTodo(@Param('id') id, @Body() todoData: Partial<Todo>) : Todo{
+    //<partial> on utilise si il passe une partie de l'objet dans le body
+    const todo = this.todos.find((todo : Todo) => todo.id === id);
+    if (todo.name != todoData.name){
+       todo.name=todoData.name;
+    }
+    if (todo.description != todoData.description){
+      todo.description = todoData.description;
+    }
+   return todo;
+  }
+
+
+
 }
